eb07519f2f538ed86ae0a1e74cb93b6523886f38
Mark Wilson <mwilsonrev@gmail.com>
2021-01-11 19:07:35 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Angela";
+const luckyNumber = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Your lucky number is {luckyNumber}.</p>
+  </div>,
+  document.getElementById("root")
+);
